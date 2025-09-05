608837eef94c61820a18ce866b415a1d45c7ad61
nikitqa <nikdobr25@gmail.com>
2025-09-05 19:47:05 +0300

    it was problem with path index.htlm

--- main.py
+++ main.py
@@ -1,15 +1,24 @@
 from fastapi import FastAPI
 from fastapi.responses import FileResponse
+from pydantic import BaseModel
 
 app = FastAPI()
 
+# class Calc(BaseModel):
+
+#     num1: int
+#     num2: int
+
 
 @app.get("/")
-async def get_info():
+async def into_file():
     return FileResponse("public/index.html")
 
+@app.get("/info")
+async def get_info():
+    return {"message": "Это мой первый проект FastAPI!"}
 
 @app.post("/calculate")
 async def calc(num1, num2):
-    sum = sum(num1 + num2)
+    sum = num1 + num2
     return {"message": "Сумма " + num1 + " и " + num2 + " равна " + sum}
