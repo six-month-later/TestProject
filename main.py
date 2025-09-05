7ad76fc7bfbd27037f4081b9c62f87bc56e7ef85
nikitqa <nikdobr25@gmail.com>
2025-09-05 19:26:54 +0300

    fix virtual error

--- main.py
+++ main.py
@@ -5,5 +5,11 @@ app = FastAPI()
 
 
 @app.get("/")
-async def root():
+async def get_info():
     return FileResponse("public/index.html")
+
+
+@app.post("/calculate")
+async def calc(num1, num2):
+    sum = sum(num1 + num2)
+    return {"message": "Сумма " + num1 + " и " + num2 + " равна " + sum}
