878a28b4c17eb28208178404dfc638b1ca6bd91f
nikitqa <nikdobr25@gmail.com>
2025-09-08 11:05:08 +0300

    add schemas

---
+++ schemas.py
@@ -0,0 +1,23 @@
+from pydantic import BaseModel, ConfigDict
+
+
+class Calculator(BaseModel):
+
+    num1: int
+    num2: int
+
+
+class STaskAdd(BaseModel):
+
+    name: str
+    description: str | None = None
+
+
+class STask(STaskAdd):
+
+    id: int
+    model_config = ConfigDict(from_attributes=True)
+
+
+class STaskID(BaseModel):
+    id: int 
